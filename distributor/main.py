e72657b277df05d2181acb0ce993852e68b7f829
Ihar <igor.xoxolko@gmail.com>
2021-01-08 12:44:49 +0300

    add communication with TG

--- distributor/main.py
+++ distributor/main.py
@@ -2,13 +2,24 @@ import random
 import telebot
 import os
 import requests
+import os
+import subprocess
+import json
 
 
-URL = 'http://localhost:5000/new_user'
+
+URL = 'high_load_analyzer_1:5000/new_user'
 TOKEN = os.environ['TOKEN']
 
 bot = telebot.TeleBot(TOKEN)
 
+
+# process = subprocess.Popen(['curl', '--header', '"Content-Type: application/json"',
+#                             '--request', 'POST', ' --data', f'\'{json.dumps(myobj)}\'', URL ],
+#                      stdout=subprocess.PIPE,
+#                      stderr=subprocess.PIPE)
+# stdout, stderr = process.communicate()
+# print(stdout, '\n', stderr)
 
 
 @bot.message_handler(commands=['start'])
@@ -18,8 +29,9 @@ def start_message(message):
 
 @bot.message_handler(content_types=['text'])
 def process(message):
-    myobj = {"id": random.randint(10, 555555), "name": "Tel", "nickname": f"{random.randint(10, 555555)}", "language_code": "ru", "is_bot": 1}
-    x = requests.post(URL, data=myobj)
+    print(message)
+    myobj = {"id": random.randint(10, 555555), "name": "Tel", "nickname": f"{random.randint(10, 555555)}", "language_code": "ru", "is_bot": True}
+    os.system( f"""  curl --header "Content-Type: application/json"   --request POST   --data '{json.dumps(myobj)}' {URL}""")
 
 
 bot.polling()
